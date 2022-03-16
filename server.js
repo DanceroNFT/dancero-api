c97568fade33d15b393d177ecebb79e8f767d80c
smarinrojas <nafhta@gmail.com>
2022-03-16 13:48:02 -0500

    sendgrid & mongoose integration

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const sendgrid = require('@sendgrid/mail');
+const mongoose = require('mongoose');
 
 require('dotenv').config()
+
+sendgrid.setApiKey(process.env.SENDGRID_APIKEY);
 
 var port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(
-  cors({
-    origin: process.env.DANCERO_HOST,
-  })
+  cors()
 )
 
 app.set('view engine', 'ejs');
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+mongoose.connect('mongodb+srv://dancero:y5WSi0awpJVWenlI@cluster0.vquxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const Data = mongoose.model('data', { city: String });
 
 app.get('/', async(req, res)=> {
 
@@ -30,6 +34,14 @@ app.get('/success', async(req, res) =>
 app.get('/failedpayment', async(req, res) => 
 {
     res.send("Failed payment");
+});
+
+app.post('/savedata', (req, res) => {
+
+  var dataJSON = req.body.data;
+  const data = new Data({ city: dataJSON['City'] });
+  data.save().then(() => console.log('data inserted'));
+  res.sendStatus(200);
 });
 
 app.post("/create-checkout-session", async (req, res) => {
@@ -55,11 +67,40 @@ app.post("/create-checkout-session", async (req, res) => {
       cancel_url:  process.env.DANCERO_API_FAILED
 
     })
+
+    sendMail(req.body.data);
     res.json({ url: session.url })
   } catch (e) {
     res.status(500).json({ error: e.message })
   }
 })
+
+app.post('/sendmail', async (req , res) => 
+{
+  var data = req.body.data;
+  sendMail(data);
+  res.sendStatus(200);
+})
+
+
+function sendMail(data)
+{  
+  const msg = {
+    to: process.env.NOTIFICATION_MAIL,
+    from: 'santiago@testiando.co',
+    subject: 'Buy notification',
+    text:  JSON.stringify(data),
+    html: '<strong>' +  JSON.stringify(data) + '</strong>',
+ }
+ sendgrid
+    .send(msg)
+    .then((resp) => {
+      console.log('Email sent\n', resp)
+    })
+    .catch((error) => {
+      console.error(error)
+  })
+}
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
